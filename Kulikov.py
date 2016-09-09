eb5605c7557032e72a4c5ed60c39dae98babab8f
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-09 14:34:26 +0300

    kulikov

--- Kulikov.py
+++ Kulikov.py
@@ -1,4 +1,7 @@
 __author__ = 'Kulikov'
 import numpy as np
-x=input()
-y=np.log
+import matplotlib.pyplot as plt
+
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(x, x**2-x-6)
+plt.show()
---
+++ KulikovIA.py
@@ -0,0 +1,22 @@
+__author__ = 'student'
+import math
+import pylab
+from matplotlib import mlab
+
+xmin = -20.0
+xmax = 20.0
+
+dx = 0.01
+xlist = mlab.frange (xmin, xmax, dx)
+
+pylab.ion()
+
+for n in range (50):
+    ylist = [math.sin (x + n / 2.0) for x in xlist]
+    pylab.clf()
+    pylab.plot (xlist, ylist)
+    pylab.draw()
+    pylab.pause(0.3)
+
+
+pylab.close()
